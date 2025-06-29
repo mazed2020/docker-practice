b5e519ac2d55bb6fe30f189e61fa83f24b6198b8
mazed2020 <mazedulislam959@gmail.com>
2025-06-30 01:44:52 +0600

    it working docker

--- backend/routes/router.js
+++ backend/routes/router.js
@@ -1,14 +1,13 @@
 import express from "express";
-import {createUser}  from "../controllers/user.controller.js";
-import {userRead} from "../controllers/user.controller.js";
+import { createUser}  from "../controllers/user.controller.js";
+import {userRead,teasting} from "../controllers/user.controller.js";
  
 const router = express.Router();
  
 router.route("/userRead").get(userRead);
 router.post("/createUser",createUser
 );
-router.get("/",(req,res)=>{
-    res.send("hello world");
-})
+router.get("/read",teasting);
+
  
 export default router;
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,8 +2,13 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
  export default defineConfig({
   server:{
-    host: '0.0.0.0',
-    port:5173
+    host:"0.0.0.0",
+    port:5173,
+    watch:{
+      usePolling:true,
+      interval:100
+    }
   },
+ 
   plugins: [react()],
 })
